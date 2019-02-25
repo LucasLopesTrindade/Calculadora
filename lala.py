57a665146ee66d7fe847ef526bb35f2de509be48
Lucas Lopes Trindade <lucaslotrindadee@gmail.com>
2019-02-25 15:35:38 -0300

    adicionando a calculado no modo multiplas paginas

--- lala.py
+++ lala.py
@@ -1,90 +1,90 @@
 # The code for changing pages was derived from: http://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
 # License: http://creativecommons.org/licenses/by-sa/3.0/   
 
-import tkinter as tk
-
+from tkinter import *
+import parser
+import math
 
 LARGE_FONT= ("Verdana", 12)
 
+pos=0
 
-class SeaofBTCapp(tk.Tk):
+
+class main_calculator(Tk):
 
     def __init__(self, *args, **kwargs):
         
-        tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self)
-
+        Tk.__init__(self, *args, **kwargs)
+        container = Frame(self)
         container.pack(side="top", fill="both", expand = True)
-
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo):
-
+        for F in (basic_calc, cientific_calc):
             frame = F(container, self)
-
             self.frames[F] = frame
-
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(StartPage)
+        self.show_frame(basic_calc)
 
     def show_frame(self, cont):
 
         frame = self.frames[cont]
         frame.tkraise()
 
-        
-class StartPage(tk.Frame):
+    # Função genérica de clique
+    def clique():
+        print('vc clicou')
+
+    # Insere numeros no display
+    def insert_num(num):    
+        global pos 
+        if pos == 0:
+            display.delete(0,END) 
+        display.insert(pos,num)
+        invisible_display.insert(pos,num)
+        if num == '**(1/2)':
+            pos+=6
+        else:
+            pos+=1
+            
+    # Limpa o display ao clicar em 'AC'
+    def clear_display():
+        display.delete(0,END)
+        display.insert(0,0)
+        global pos
+        pos=0
+
+    def calculate():
+        result_string = display.get()
+        invisible_result = invisible_display.get()
+        print(invisible_result)
+        a = parser.expr(result_string).compile()
+        result = eval(a)
+        display.delete(0,END)
+        display.insert(0,result)
+        global pos
+        pos=0
+            
+class basic_calc(Frame):
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        Frame.__init__(self,parent)
+        label = Label(self, text="Calculator", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
-
-        button = tk.Button(self, text="Visit Page 1",
-                            command=lambda: controller.show_frame(PageOne))
+        button = Button(self, text="Visit Page 1",command=lambda: controller.show_frame(cientific_calc))
         button.pack()
 
-        button2 = tk.Button(self, text="Visit Page 2",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.pack()
-
-
-class PageOne(tk.Frame):
+class cientific_calc(Frame):
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Page One!!!", font=LARGE_FONT)
+        Frame.__init__(self, parent)
+        label = Label(self, text="Page One!!!", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
-
-        button1 = tk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
+        button1 = Button(self, text="Back to Home",command=lambda: controller.show_frame(basic_calc))
         button1.pack()
 
-        button2 = tk.Button(self, text="Page Two",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.pack()
 
-
-class PageTwo(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-
-        button1 = tk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-        button2 = tk.Button(self, text="Page One",
-                            command=lambda: controller.show_frame(PageOne))
-        button2.pack()
-        
-
-
-app = SeaofBTCapp()
+app = main_calculator()
 app.mainloop()
